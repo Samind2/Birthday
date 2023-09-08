834b0e0e6b947ba6e7a5a3e61af9928d0122f913
Samind2 <111554627+Samind2@users.noreply.github.com>
2023-09-08 12:03:47 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import data from './data'
+
+
+function App() {
+ 
+
+  return (
+  
+      <div>
+        
+      </div>
+      
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        id: 1,
+        name: "Sinde",
+        age: 20,
+        Image:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    }
+    {
+        id: 2,
+        name: "Naming",
+        age: 20,
+        Image:"https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1522&q=80",
+    }
+    {
+        id: 3,
+        name: "Alel",
+        age: 20,
+        Image:"https://plus.unsplash.com/premium_photo-1678197937465-bdbc4ed95815?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80",
+    }
+    {
+        id: 4,
+        name: "Mark",
+        age: 20,
+        Image:"https://images.unsplash.com/photo-1674704740461-3a49bdedfeef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80",
+    }
+    {
+        id: 5,
+        name: "Amanda",
+        age: 20,
+        Image:"https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1588&q=80",
+    }
+];
+
+export default data;
